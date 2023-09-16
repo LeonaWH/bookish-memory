9dd75ff911b0e283e3b11c5e447385bd9067b015
Leona Varghese <leonavarghesewh@gmail.com>
2023-09-16 22:01:45 +0530

    Features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components';
+
+const featuresData = [
+    {
+        title: 'Improving end distrusts instantly',
+        text: 'Lorem Ipsum has been the industrys standard dummy text ever since '
+    },
+    {
+        title: 'Become the tended active',
+        text: 'Lorem Ipsum has been the industrys standard dummy text ever since '
+    },
+    {
+        title: 'Message or am nothing',
+        text: 'Lorem Ipsum has been the industrys standard dummy text ever since '
+    },
+    {
+        title: 'Step into the Future',
+        text: 'Lorem Ipsum has been the industrys standard dummy text ever since '
+    }
+
+]
 
 const Features = () => {
-    return(<div>
-        Features
+    return(
+    <div className='gpt3__features section__padding' id ="features">
+    <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'> The Future is now and you just need to realize it. Step into the future today & make it happen.</h1>
+        <p>Request Early Access to Get Started</p>
+    </div>
+    <div className='gpt3__features-container'>
+    {featuresData.map((item, index) =>(
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+    ))}
+
+    </div>
+      
     </div>)
 }
 export default Features;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './whatGPT3.css';
+import Feature from '../../components/feature/Feature';
 
 const WhatGPT3 = () => {
     return (
     <div className='gpt3__whatgpt3 section margin' id="wgpt3">
     <div className='gpt3__whatgpt3-feature'>
-       
+    <Feature title="What is GPT-3?" text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
     </div>
-       
+    <div className='gpt3__whatgpt3-heading'>
+    <h1 className='gradient__text'>The possibilities are beyond your imagination </h1>
+    <p> Explore the Library</p>
+    </div>
+    <div className='gpt3__whatgpt3-container'>
+    <Feature title="Chatbots" text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+    <Feature title="Knowledgebase" text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+    <Feature title="Education" text="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
+    </div>  
     </div>)
 }
 export default WhatGPT3;
